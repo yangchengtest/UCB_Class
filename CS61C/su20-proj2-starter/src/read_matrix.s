32406d4e8ab208efb9fbfc8a6531a213a6278fd5
magicyang <yangchengtest@github.com>
2021-01-23 16:13:08 +0800

    Proj-2

--- CS61C/su20-proj2-starter/src/read_matrix.s
+++ CS61C/su20-proj2-starter/src/read_matrix.s
@@ -26,16 +26,85 @@
 read_matrix:
 
     # Prologue
-	
+	addi sp,sp,-28
+	sw s0,0(sp)
+	sw s1,4(sp)
+	sw s2,8(sp)
+	sw s3,12(sp)
+	sw s4,16(sp)
+	sw s5,20(sp)
+	sw ra,24(sp)
+    
+    mv s0,a0
+    mv s1,a1
+    mv s2,a2
 
+    #fopen
+    mv a1,s0
+    li a2,0
+    jal fopen
+    li t0,-1
+    beq a0,t0,exit_50
 
+    #file handle
+    mv s0,a0
 
+    #load row
+    mv a1,s0
+    mv a2,s1
+    li s3,4
+    mv a3,s3
+    jal fread
+    bne a0,s3,exit_51
 
+    #load col
+    mv a1,s0
+    mv a2,s2
+    mv a3,s3
+    jal fread
+    bne a0,s3,exit_51
 
+    #malloc 
+    lw t0,0(s1)
+    lw t1,0(s2)
+    mul t2,t0,t1
+    mul s4,t2,s3 #memory size
+    mv a0,s4
+    jal malloc
+    mv s5,a0 #memory point
 
+    #malloc read
+    mv a1,s0
+    mv a2,s5
+    mv a3,s4
+    jal fread
+    bne a0,s4,exit_51
 
+    #close file
+    mv a1,s0
+    jal fclose
+    bne a0,x0,exit_52
 
     # Epilogue
-
-
+    mv a0,s5
+    lw s0,0(sp)
+    lw s1,4(sp)
+    lw s2,8(sp)
+    lw s3,12(sp)
+    lw s4,16(sp)
+    lw s5,20(sp)
+    lw ra,24(sp)
+    addi sp,sp,28
     ret
+
+exit_50:
+    li a1, 50
+    j exit2
+
+exit_51:
+    li a1, 51
+    j exit2
+
+exit_52:
+    li a1, 52
+    j exit2
--- CS61C/su20-proj2-starter/src/write_matrix.s
+++ CS61C/su20-proj2-starter/src/write_matrix.s
@@ -1,5 +1,4 @@
 .globl write_matrix
-
 .text
 # ==============================================================================
 # FUNCTION: Writes a matrix of integers into a binary file
@@ -28,15 +27,85 @@ write_matrix:
 
     # Prologue
 
+    addi sp,sp,-24
+    sw s0,0(sp)
+    sw s1,4(sp)
+    sw s2,8(sp)
+    sw s3,12(sp)
+    sw ra,16(sp)
 
+    mv s0, a0
+    mv s1, a1
+    mv s2, a2
+    mv s3, a3
+    
+    #fopen
+    mv a1, s0
+    li a2, 1
+    jal fopen
+    li t0,-1
+    beq a0, t0, exit_53
 
+    #file handle
+    mv s0, a0
 
+    #fwrite row
+    mv a1, s0
+    addi sp,sp,-4
+    sw s2,0(sp)
+    mv a2,sp
+    li a3, 1
+    li a4, 4
+    jal fwrite
+    li t0,1
+    addi sp,sp,4
+    bne a0, t0, exit_54   
+    
+    #fwrite row
 
+    mv a1, s0
+    addi sp,sp,-4
+    sw s3,0(sp)
+    mv a2,sp
+    li a3, 1
+    li a4, 4
+    jal fwrite
+    li t0,1
+    addi sp,sp,4
+    bne a0, t0, exit_54  
+	
+	mul s2, s2, s3 #size
 
+    #fwrite matrix
+    mv a1, s0
+    mv a2, s1
+    mv a3, s2
+    li a4, 4
+    jal fwrite
+    bne a0, s2, exit_54
 
-
+    #fclose
+    mv a1, s0
+    jal fclose
+    bne a0, x0, exit_55
 
     # Epilogue
-
-
+    lw s0,0(sp)
+    lw s1,4(sp)
+    lw s2,8(sp)
+    lw s3,12(sp)
+    lw ra,16(sp)
+    addi sp,sp,20
     ret
+
+exit_53:
+    li a1, 53
+    j exit2
+
+exit_54:
+    li a1, 54
+    j exit2
+
+exit_55:
+    li a1, 55
+    j exit2
